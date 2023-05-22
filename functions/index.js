5c6c8182f6bfb63728ada3d220d33030ca93a14d
Matej Kuka <mate1778@easv365.dk>
2023-05-22 15:32:22 +0200

    finished sending emails

--- functions/index.js
+++ functions/index.js
@@ -7,40 +7,22 @@ const cors = require("cors")({origin: true});
 admin.initializeApp();
 
 const transporter = nodemailer.createTransport({
-    //service: "gmail",
     host: 'smtp.gmail.com',
     port: 587,
     secure: false,
     auth: {
-        user: "onlinephotoalbum@gmail.com",
-        pass: "testingTest123",
+        user: "onlinephotoalbumproject@gmail.com",
+        pass: "twezdtwmxtrtkhcs",
     },
 });
 
 exports.sendWelcomingMail = functions.auth.user().onCreate((user, context) => {
 
     const mailOptions = {
-        from: "OPAP <onlinephotoalbum@gmail.com>",
+        from: "OPAP <onlinephotoalbumproject@gmail.com>",
         to: user.email,
         subject: "Welcome",
         html: `Thanks in our Online Photo Album Project. Your account is successfully created.`,
-    };
-
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.log(error.toString());
-        }
-        console.log("Sended");
-    })
-});
-
-exports.sendByeMail = functions.auth.user().onDelete((user, context) => {
-
-    const mailOptions = {
-        from: "OPAP <onlinephotoalbum@gmail.com>",
-        to: user.email,
-        subject: "Bye",
-        html: `Your account is successfully deleted.`,
     };
 
     transporter.sendMail(mailOptions, (error, info) => {
